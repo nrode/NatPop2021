3c03a3c486b51f5bb8fac5fbc686a0b3e3fbcfec
Nicolas O. Rode <nicolas.rode@inrae.fr>
2021-01-25 15:39:33 +0100

    :dog: document function

--- devhistory.R
+++ devhistory.R
@@ -12,5 +12,20 @@ usethis::use_git(message = ":see_no_evil: Ban .DS_Store files")
 usethis::edit_file("DESCRIPTION")
 usethis::use_git(message = ":bulb: Update documentation")
 
+## Create directories
+dir.create("data")
+dir.create("reports")
+
+## Update DESCRIPTION file
+usethis::use_package("here")
+
 ## Create a R directory and a file for functions
 usethis::use_r("simulmodel")
+
+
+
+## Update NAMESPACE file
+devtools::document()
+
+## Load all required packages
+devtools::load_all()
