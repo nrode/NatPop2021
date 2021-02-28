96be037763053c6632aaf6c89a17459a83a9624f
Laure Olazcuaga <laure@Laures-MacBook-Pro.local>
2021-02-27 21:35:26 -0700

    :zebra: analysis data and plot of the reciprocal transplant experiment

---
+++ R/plot_RTP_residuals.R
@@ -0,0 +1,90 @@
+#' Plot for reciprocal transplant experiment
+#'
+#' @description Create plot for reciprocal transplant experiment
+#' @param gen the generation of dataset
+#' @param trait the trait 
+#' 
+#' @return plot of the residuals of the trait on the three environement
+#' @export 
+#'
+#' @examples
+#'plot_RTP_residuals(dataset = data_PERF, trait = "Nb_eggs", gen = "G0")
+
+
+plot_RTP_residuals <- function(dataset = data_PERF, trait = "Nb_eggs", gen = "G0"){
+ 
+  # Subset dataset
+  data <- dataset[dataset$Generation == gen,]
+  data <- data[complete.cases(data[,trait]), ]
+  
+  
+  # Transform variables
+  if(trait == "Nb_eggs" | trait == "Nb_adults" ){
+    data$y <- log(data[,trait]+1)
+  }else{
+    if(trait == "Rate"){
+      data$y <- asin(sqrt(data[,trait]))
+        }else{
+          print("Error: unknown trait")
+        }
+    }
+
+  #Extract resid 
+  lm_resid <- lm(y ~ Test_environment + Population, data=data)
+  data$Resid <- residuals(lm_resid)
+  
+  ##Dataset summary
+  TEMP_SUM <- Rmisc::summarySE(data,
+                                  measurevar="Resid",
+                                  groupvars=c("Original_environment","Test_environment","SA"))
+  
+  pd <- position_dodge(0.6) # move them .05 to the left and right
+  
+  # Plot title and y axis title
+  plot_title <- ifelse(gen == "G0", "First generation", "Third generation")
+  if("Obs_A" %in% colnames(dataset)  & trait == "Nb_eggs"){
+    yaxis_labelprint <- paste0("Standardized number of eggs")
+  }else{
+    if("Obs_A" %in% colnames(dataset)  & trait == "Nb_adults"){
+      yaxis_labelprint <- paste0("Standardized number of adults")
+    }else{
+      if("Rate" %in% colnames(dataset) && trait == "Rate"){
+        yaxis_labelprint <- paste0("Standardized emergence rate")
+      }else{
+        if("BoxID" %in% colnames(dataset) && trait == "Nb_eggs"){
+          yaxis_labelprint <- paste0("Standardized preference")
+        }else{
+        print("Error: unknown combinaison dataset x trait")
+        }
+      }
+    }
+  }
+  
+    plot <- ggplot(TEMP_SUM, aes(x = Test_environment, y = Resid,
+                               colour = Original_environment,
+                               group = Original_environment,
+                               fill = "white")) + 
+    geom_errorbar(aes(ymin=Resid-ci, ymax = Resid+ci),
+                  width=.1, position=pd, size = 1) +
+    geom_point(size = 4, position=pd, fill="white", shape = 21, stroke = 1.5) + 
+    scale_color_manual(name="Fly populations from:",   
+                       breaks=c("Cherry", "Strawberry","Blackberry"),
+                       labels=c("Cherry","Strawberry","Blackberry"),
+                       values=c("#BC3C6D","#3FAA96", "#301934")) +
+    ylab(yaxis_labelprint)  +
+    xlab("Test environment")  +
+    ggtitle(plot_title) +
+    theme_LO_sober 
+    
+    ### Add stroke
+    plot2 <- plot + 
+      geom_point(aes(alpha = SA, fill = interaction(SA, Original_environment)), 
+                 position = pd, size = 4) + 
+      scale_alpha_manual(values = c(1,0)) + 
+      guides(fill = FALSE, alpha = FALSE) 
+
+    
+  return(plot2) 
+}
+  
+  
