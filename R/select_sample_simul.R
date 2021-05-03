015f333857d34381212909e4bfa3c01b36e41a0c
olazlaure <olaz.laure@gmail.com>
2021-05-03 14:56:25 -0600

    :sheep: change for Intensity of SA (instead of proportion of interaction explained by SA)

--- R/select_sample_simul.R
+++ R/select_sample_simul.R
@@ -20,31 +20,26 @@ select_sample_simul <- function(dataset_sim = sim, type_SA = "genetic",
   if (type_SA == "genetic") {
     
   # Calculate the highest SA value with at least  nb_simul_per_SA simuls
-  #####val_SA_available_MAX<-max(dataset_sim$SA_Gen[dataset_sim$index_SA_Gen==nb_simul_per_SA])
-  val_rs_available_MAX<-max(dataset_sim$binnedrsqgen[dataset_sim$index_SA_Gen==nb_simul_per_SA])
-  
+  val_SA_available_MAX<-max(dataset_sim$SA_Gen[dataset_sim$index_SA_Gen==nb_simul_per_SA])
+
   
   # Calculate the lowest SA value with at least nb_simul_per_SA simuls
   if (minimum_SA == "select") {
-    #####val_SA_available_MIN<-min(dataset_sim$SA_Gen[dataset_sim$index_SA_Gen==nb_simul_per_SA])
-    val_rs_available_MIN<-min(dataset_sim$binnedrsqgen[dataset_sim$index_SA_Gen==nb_simul_per_SA])
-    
+    val_SA_available_MIN<-min(dataset_sim$SA_Gen[dataset_sim$index_SA_Gen==nb_simul_per_SA])
+
   }else{
     if (minimum_SA == "0") {
-      #####val_SA_available_MIN <- 0
-      val_rs_available_MIN <- 0
-      
+      val_SA_available_MIN <- 0
+
     }else{
       print("Error: minimum_SA unknown")  
       }
     }
   
   # Select simuls with the SA value with at least nb_simul_per_SA simuls
-  #####select_dataset_sim<-dataset_sim[dataset_sim$SA_Gen>=val_SA_available_MIN&
-  #####dataset_sim$SA_Gen<=val_SA_available_MAX,]
-  select_dataset_sim<-dataset_sim[dataset_sim$binnedrsqgen>=val_rs_available_MIN&
-                                    dataset_sim$binnedrsqgen<=val_rs_available_MAX,]
-  
+  select_dataset_sim<-dataset_sim[dataset_sim$SA_Gen>=val_SA_available_MIN&
+  dataset_sim$SA_Gen<=val_SA_available_MAX,]
+
   
   # Keep only the same number of simul (=nb_simul_per_SA) per SA value
   select_dataset_sim<-select_dataset_sim[select_dataset_sim$index_SA_Gen<=nb_simul_per_SA,]
@@ -54,33 +49,27 @@ select_sample_simul <- function(dataset_sim = sim, type_SA = "genetic",
     if (type_SA == "non-genetic") {
   
       # Calculate the highest SA value with at least  nb_simul_per_SA simuls
-      #####val_SA_available_MAX<-max(dataset_sim$SA_NonGen[dataset_sim$index_SA_NonGen==nb_simul_per_SA])
-      val_rs_available_MAX<-max(dataset_sim$binnedrsqng[dataset_sim$index_SA_NonGen==nb_simul_per_SA])
-      
+      val_SA_available_MAX<-max(dataset_sim$SA_NonGen[dataset_sim$index_SA_NonGen==nb_simul_per_SA])
+
       
       # Calculate the lowest SA value with at least nb_simul_per_SA simuls
       if (minimum_SA == "select") {
-        ##### val_SA_available_MIN<-min(dataset_sim$SA_NonGen[dataset_sim$index_SA_NonGen==nb_simul_per_SA])
-        val_rs_available_MIN<-min(dataset_sim$binnedrsqng[dataset_sim$index_SA_NonGen==nb_simul_per_SA])
-        
+        val_SA_available_MIN<-min(dataset_sim$SA_NonGen[dataset_sim$index_SA_NonGen==nb_simul_per_SA])
+
       }else{
         if (minimum_SA == "0") {
-          #####val_SA_available_MIN <- 0
-          val_rs_available_MIN <- 0
-          
+          val_SA_available_MIN <- 0
+
         }else{
           print("Error: minimum_SA unknown")  
         }
       }
   
       # Select simuls with the SA value with at least nb_simul_per_SA simuls
-      #####select_dataset_sim<-dataset_sim[dataset_sim$SA_NonGen>=val_SA_available_MIN&
-      #####                   dataset_sim$SA_NonGen<=val_SA_available_MAX,]
-      
-      select_dataset_sim<-dataset_sim[dataset_sim$binnedrsqng>=val_rs_available_MIN&
-                                        dataset_sim$binnedrsqng<=val_rs_available_MAX,]
-      
-      
+      select_dataset_sim<-dataset_sim[dataset_sim$SA_NonGen>=val_SA_available_MIN&
+                        dataset_sim$SA_NonGen<=val_SA_available_MAX,]
+
+
       # Keep only the same number of simul (=nb_simul_per_SA) per SA value
       select_dataset_sim<-select_dataset_sim[select_dataset_sim$index_SA_NonGen<=nb_simul_per_SA,]
       
